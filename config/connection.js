3be2d33c5674af1157a445623a27960986972d1d
Chase Holladay <chaseholladay@gmail.com>
2018-10-30 14:36:35 -0500

    connected to heroku jaws

--- config/connection.js
+++ config/connection.js
@@ -8,12 +8,11 @@ if (process.env.JAWSDB_URL) {
 } else {
 	connection = mysql.createConnection({
 		host: "localhost",
-		port: 3306,
 		user: "root",
 		password: "root",
 		database: "burgers_db"
 	});
-}
+};
 
 connection.connect(function(err) {
 	if (err) {
